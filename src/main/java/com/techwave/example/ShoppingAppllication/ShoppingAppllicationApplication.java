cfa4df719def46ff3c5424196cfd3896ef9cf90d
Mani Tharun Kotagiri <11202110@in.techwave.net>
2021-07-19 18:05:02 +0530

    first commit

---
+++ src/main/java/com/techwave/example/ShoppingAppllication/ShoppingAppllicationApplication.java
@@ -0,0 +1,13 @@
+package com.techwave.example.ShoppingAppllication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingAppllicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingAppllicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techwave/example/ShoppingAppllication/ShoppingAppllicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techwave.example.ShoppingAppllication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingAppllicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
